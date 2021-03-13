1c6e39d3c93e518b36a36f58d384dea4a1f1940a
Ben <bbryant@truecoders.io>
2021-03-12 18:34:47 -0600

    Added solution to answer branch

--- app.js
+++ app.js
@@ -6,3 +6,37 @@ console.log("EXERCISE 1:\n==========\n");
 const http = require("http");
 
 // Finish setting up the server
+
+http
+  .createServer(function (request, response) {
+    let { url } = request;
+    let chunks = [];
+    request.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+    request.on("end", function () {
+      const body = Buffer.concat(chunks).toString();
+
+      const wildcard = {
+        brand: "Wrigley's",
+        flavor: "Juicy Fruit",
+      };
+
+      const self = {
+        name: "Ben",
+        favColor: "red",
+      };
+
+      if (url === "/") {
+        response.write(JSON.stringify(wildcard));
+      } else if (url === "/about") {
+        response.write(JSON.stringify(self));
+      } else if (url === "/echo") {
+        response.write(body);
+      }
+      response.end();
+    });
+  })
+  .listen(3000, function () {
+    console.log("Server listening on port 3000...");
+  });
